0915ccfae343f714edf2bea9108308d25837f3f2
LegallyKF5RCL <kf5rcl@gmail.com>
2015-05-04 15:37:43 -0500

    Added MathFilter

--- Development.m
+++ Development.m
@@ -6,8 +6,8 @@ format shorteng;
 % RecordRate = 44100;
 
 %Pointer to recording at 44.1kHz
-FilePointerNoise44 = 'C:\Users\KF5RCL\Desktop\MyStuff\Classes\Discrete\Project\Audio\NoiseAt44k_Bridge.wav';
-FilePointerSpeech44 = 'C:\Users\KF5RCL\Desktop\MyStuff\Classes\Discrete\Project\Audio\PatriceOneal.wav';
+FilePointerNoise44 = 'C:\Users\KF5RCL\Desktop\MyStuff\Classes\Discrete\DiscreteFinalProject\Audio\AudioTracks\Bridge\NoiseAt44k_Bridge.wav';
+FilePointerSpeech44 = 'C:\Users\KF5RCL\Desktop\MyStuff\Classes\Discrete\DiscreteFinalProject\Audio\AudioTracks\PatriceOneal.wav';
 %Read in the noise recording
 [Noise44, RecordRate] = audioread(FilePointerNoise44);
 [Signal44, RecordRate] = audioread(FilePointerSpeech44);
@@ -49,31 +49,35 @@ SpectrumVector = zeros(1, Fs * 10);
 
 Recover = ifft(SigSpec);
 
-sound(Recover, 36000);
+% sound(Recover, 36000);
 
-% SignalLength = size(Noise);         %find the length of the signal
-% SignalLength = SignalLength(1,2);   %extract useful info
-%Noise = 1:1:Length;        %test if loop works
+SignalLength = size(Noise);         %find the length of the signal
+SignalLength = SignalLength(1,2);   %extract useful info
+% Noise = 1:1:Length;        %test if loop works
 
-%Truncate the noise signal into 24 samples of 10 seconds
-% for kk = 1:24
-%     for jj = 1:(Fs * 10)
-%         NoiseMatrix(kk, jj) = Noise(1, (kk-1)*Fs + jj);
-%     end
-% end
-% 
-% for kk = 1:24
-%         SpectrumVector(1,:) = SpectrumVector(1,:) + fftshift(fft(NoiseMatrix(kk,:), Fs * 10));
-% end
-% 
-% SpectrumVector(1,:) = SpectrumVector(1,:) / 24;
-% 
-% Mag = abs(SpectrumVector);
-% Pha = angle(SpectrumVector);
-% subplot(2,1,1);
-% stem(f, Mag);
-% subplot(2,1,2);
-% plot(f, Pha);
+% Truncate the noise signal into 24 samples of 10 seconds
+for kk = 1:24
+    for jj = 1:(Fs * 10)
+        NoiseMatrix(kk, jj) = Noise(1, (kk-1)*Fs + jj);
+    end
+end
+
+for kk = 1:24
+        SpectrumVector(1,:) = SpectrumVector(1,:) + fftshift(fft(NoiseMatrix(kk,:), Fs * 10));
+end
+
+SpectrumVector(1,:) = SpectrumVector(1,:) / 24;
+
+Mag = abs(SpectrumVector);
+Pha = angle(SpectrumVector);
+
+f = linspace(-Fs/2, Fs/2, Fs * 10);
+
+figure;
+subplot(2,1,1);
+stem(f, Mag);
+subplot(2,1,2);
+plot(f, Pha);
 
 
 
